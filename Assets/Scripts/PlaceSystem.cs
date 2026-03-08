66afed4677b6f38bb0e74bd6911bd75c1c8af39c
Raiden <2139799373@qq.com>
2026-03-08 13:24:56 +0800

    update:实现建筑建造切换

--- Assets/Scripts/PlaceSystem.cs
+++ Assets/Scripts/PlaceSystem.cs
@@ -4,9 +4,12 @@ using UnityEngine.Tilemaps;
 public class PlaceSystem : MonoBehaviour
 {
     public Grid grid;               // 场景中的 Grid 组件
-    public GameObject buildingPrefab;   // 你想要放置的建筑预制体
+    public GameObject guanfu;
+    public GameObject minju;
     public Tilemap buildingTilemap; // 专门用来放建筑的 Tilemap 层
-    public TileBase factoryTile;    // 你在 Tile Palette 里创建好的“建筑” Tile
+    public TileBase minjuTile;    // 你在 Tile Palette 里创建好的“建筑” Tile
+    public TileBase guanfuTile;   // 另一个建筑 Tile
+    private TileBase currentTile;    // 当前选中的建筑 Tile
     private GameObject currentInstance;
 
     public GameObject ConstructionPanel; // 建造面板
@@ -14,17 +17,32 @@ public class PlaceSystem : MonoBehaviour
 
     void Awake()
     {
-        currentInstance = Instantiate(buildingPrefab);
+        currentInstance = Instantiate(minju); // 默认放置民居
+        Instantiate(guanfu);
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab)) // 按 B 键打开建造面板
         {
-            isPlacing = true;
-            ConstructionPanel.SetActive(true);
+            isPlacing = !isPlacing;
+            ConstructionPanel.SetActive(isPlacing);
+            currentInstance.SetActive(isPlacing); // 只有在放置模式下才显示预览建筑
+            currentTile = minjuTile; // 默认选中民居
         }
         if (currentInstance == null || !isPlacing) return;
 
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // 按 1 键切换到民居
+        {
+            Destroy(currentInstance);
+            currentInstance = Instantiate(minju);
+            currentTile = minjuTile;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) // 按 2 键切换到官府
+        {
+            Destroy(currentInstance);
+            currentInstance = Instantiate(guanfu);
+            currentTile = guanfuTile;
+        }
         // 1. 获取鼠标世界坐标
         Vector3 mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Mathf.Abs(Camera.main.transform.position.z);
@@ -53,7 +71,7 @@ public class PlaceSystem : MonoBehaviour
     void PlaceBuilding(Vector3Int position)
     {
         // 调用 API 在指定位置放置 Tile
-        buildingTilemap.SetTile(position, factoryTile);
-        Debug.Log($"在 {position} 建造了物品");
+        buildingTilemap.SetTile(position, currentTile);
+        Debug.Log($"在 {position} 建造了 {currentTile.name}！");
     }
 }
